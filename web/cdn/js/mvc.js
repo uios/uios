0dcea456bc6c68feda2c3acea473f00a0977707f
Tim Banon <3849997+thebanon@users.noreply.github.com>
2021-01-10 14:54:40 -0500

    Update Notes UI

--- web/cdn/js/mvc.js
+++ web/cdn/js/mvc.js
@@ -10,12 +10,12 @@ window.mvc['v'] = {
 
         apps: async(app) => {
             
-            var json = JSON.parse(await ajax('/cdn/json/config/'+app+'.json')); console.log({json});
+            //var json = JSON.parse(await ajax('/cdn/json/config/'+app+'.json')); console.log({json});
 
             var html = ``;
             html += `<div class="app app-`+app+`" data-evt="app">`; //style="width:`+json.size.width+`;height:`+json.size.height+`;"
-                html += `<header><section class="name" data-after="`+json.name+`"></section><section class="actions"><a class="mini"></a><a class="maxi"></a><a class="exit"></a></section></header>`;
-                html += `<iframe allow="`+json.allow+`" src="`+window.location.protocol+`//`+app+`.`+window.location.host+`">`;
+                html += `<header><section class="name" data-after="`+app+`"></section><section class="actions"><a class="mini"></a><a class="maxi"></a><a class="exit"></a></section></header>`;
+                html += `<iframe allow="camera;microphone" src="`+window.location.protocol+`//`+app+`.`+window.location.host+`">`;
             html += `</div>`;
             byId('apps').insertAdjacentHTML('beforeend',html);
         },
