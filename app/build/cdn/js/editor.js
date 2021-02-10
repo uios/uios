84de107b78ff92a80678730fd9cdd5245a34785d
Tim Banon <3849997+thebanon@users.noreply.github.com>
2021-02-09 21:48:35 -0500

    Debug Build App

--- app/build/cdn/js/editor.js
+++ app/build/cdn/js/editor.js
@@ -7,15 +7,8 @@ window.editor = {
             var ement = target.closest('[data-element]');
             if(ement) {  
               var tagName = ement.dataset.element;
-              element = `<block class="block block-`+tagName+`">`;
-                element += `<header class="block-header header"><section><xmp><`+tagName+`></`+tagName+`></xmp></section></header>`;
-                element += `<section class="block-section `+tagName+`"></section>`;
-                element += `<footer class="block-footer insert"><section data-evt="editor">`;
-                  element += `<div class="create" data-crud="create" data-elem="section"></div>`;
-                  element += `<div class="delete" data-crud="delete" data-elem="block"></div>`;
-                element += `</section></footer>`;
-              element += `</block>`;
-              ement.closest('.page').lastElementChild.insertAdjacentHTML('beforebegin',element);   
+              var element = byId('block-div').innerHTML; 
+              ement.closest('.page').lastElementChild.insertAdjacentHTML('beforebegin',element); 
             }
         },
 
